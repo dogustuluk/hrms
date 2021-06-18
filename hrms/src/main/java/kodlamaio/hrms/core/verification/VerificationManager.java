441013f380809a9308fb73c3f0439252d646c8f7
dogus <dogus@DESKTOP-189GO9O>
2021-06-18 15:36:37 +0300

    Created "VerificationService" and "VerificationManager" module.

---
+++ hrms/src/main/java/kodlamaio/hrms/core/verification/VerificationManager.java
@@ -0,0 +1,24 @@
+package kodlamaio.hrms.core.verification;
+
+import java.util.UUID;
+
+public class VerificationManager implements VerificationService {
+
+	@Override
+	public void sendLink(String email) {
+		UUID uuid = UUID.randomUUID();
+		String verificationLink = "https://hrmsverificationmail/" + uuid.toString();
+		System.out.println("doğrulama adresi: " +email);
+		System.out.println("hesabınızı doğrulamak için bağlantıya tıklayın: " + verificationLink);
+		
+	}
+
+	@Override
+	public String sendCode() {
+		UUID uuid = UUID.randomUUID();
+		String verificationCode = uuid.toString();
+		System.out.println("aktivasyon kodunuz: "+ verificationCode);
+		return verificationCode;
+	}
+
+}
